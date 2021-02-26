a8223eee6acfc2fd85b3633bb063e2f3c00cedb5
EDITCREATION PC <s.alexandrov@editcreation.com>
2021-02-26 16:55:11 +0200

    Update eventsValidate.js

--- src/mixins/eventsValidate.js
+++ src/mixins/eventsValidate.js
@@ -10,12 +10,18 @@ export default {
 
     methods: {
         validateEventName () {
-            if (this.eventName) this.checkAllInputs();
+            if (this.eventName && this.eventName.length > 3 && this.eventName.length < 25) this.checkAllInputs();
             else this.nameErr = true;
         },
 
         validateEventDesc () {
-            if (this.eventDesc) this.checkAllInputs();
+            if (this.eventDesc && this.eventDesc.length > 5 && this.eventDesc < 100) this.checkAllInputs();
+            else this.descErr = true;
+        },
+
+        validateEventImgUrl () {
+            const imgRegex = /^https?/i
+            if (this.eventImgUrl && imgRegex.test(this.eventImgUrl)) this.checkAllInputs();
             else this.descErr = true;
         },
 
