dc963cf2c834a139b749bae8d6b0653b0dce541b
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-17 12:48:41 +0200

    'config'

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+// vue.config.js file to be place in the root of your repository
+
 module.exports = {
-    publicPath: '/organiser-app/'
-}
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/my-project/'
+      : '/'
+  }
