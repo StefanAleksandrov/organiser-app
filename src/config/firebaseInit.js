45c1b6d0bd221fa7aec23b4c387ecb4a09f05448
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-30 01:01:33 +0300

    Update firebaseInit.js

--- src/config/firebaseInit.js
+++ src/config/firebaseInit.js
@@ -1,8 +1,8 @@
-import firebase from 'firebase/app'
-import 'firebase/firebase-auth'
-import 'firebase/auth'
-import { firebaseConfig } from './firebaseConfig'
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const auth = firebaseApp.auth()
+export const auth = firebaseApp.auth();
